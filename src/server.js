b31417c6a7b2513987d081e7d19d22e3dd3b085a
Jordan Curzon <curzonj@gmail.com>
2014-10-04 21:55:07 -0700

    undock is working

--- src/server.js
+++ src/server.js
@@ -23,6 +23,7 @@ app.use(bodyParser.urlencoded({
 // have. It'll need a seperate root hash.
 var inventories = {};
 var slice_permissions = {};
+var ships = {};
 
 function getBlueprints() {
     return qhttp.read(process.env.TECHDB_URL + '/blueprints').then(function(b) {
@@ -53,7 +54,7 @@ app.delete('/containers/:uuid', function(req, res) {
 
 app.post('/containers/:uuid', function(req, res) {
     var uuid = req.param('uuid'),
-        blueprintID = req.param('blueprint');
+    blueprintID = req.param('blueprint');
 
     Q.spread([getBlueprints(), C.authorize_req(req, true)], function(blueprints, auth) {
         var blueprint = blueprints[blueprintID];
@@ -85,7 +86,7 @@ function containerAuthorized(uuid, account) {
 
 function updateContainer(uuid, newBlueprint) {
     var i = inventories[uuid],
-        b = newBlueprint;
+    b = newBlueprint;
 
     i.blueprint = newBlueprint.uuid;
     i.capacity.cargo = b.inventory_capacity;
@@ -156,21 +157,82 @@ app.get('/inventory/:uuid', function(req, res) {
     }).done();
 });
 
+app.post('/ships/:uuid', function(req, res) {
+    Q.spread([getBlueprints(), C.authorize_req(req, true)], function(blueprints, auth) {
+        var uuid = req.param('uuid');
+        var ship = ships[uuid];
+
+        console.log(ship);
+        console.log(req.body);
+
+        if (ship === undefined) {
+            return res.sendStatus(404);
+        }
+
+        delete req.body.blueprint;
+
+        var blueprint = blueprints[ship.blueprint];
+        var undock = req.body.in_space;
+        var quantity, location, slice;
+
+        if (undock !== undefined) {
+            if (undock === true) {
+                if (!containerAuthorized(ship.location, auth.account)) {
+                    return res.sendStatus(401);
+                }
+
+                executeTransfers([{
+                        ship_uuid: uuid,
+                        quantity: -1, // undock
+                        inventory: ship.location,
+                        blueprint: blueprint,
+                        slice: ship.slice
+                }]);
+            } else if (undock === false) {
+                if (!containerAuthorized(req.body.location, auth.account)) {
+                    return res.sendStatus(401);
+                }
+
+                executeTransfers([{
+                        ship_uuid: uuid,
+                        quantity: 1, // dock
+                        inventory: req.body.location,
+                        blueprint: blueprint,
+                        slice: req.body.slice
+                }]);
+            } else {
+                throw new Error("invalid in_space: %s", undock);
+            }
+        }
+
+        delete req.body.location;
+        delete req.body.slice;
+
+        C.deepMerge(req.body, ship);
+
+        res.send(ship);
+    }).done();
+});
+
 // this unpacks a ship from inventory and makes it unique
 app.post('/ships', function(req, res) {
     var uuid = uuidGen.v1(),
-        inventoryID = req.param('inventory'),
-        sliceID = req.param('slice'),
-        blueprintID = req.param('blueprint'),
-        inventory = inventories[inventoryID];
+    inventoryID = req.param('inventory'),
+    sliceID = req.param('slice'),
+    blueprintID = req.param('blueprint'),
+    inventory = inventories[inventoryID];
 
-    C.authorize_req(req).then(function(auth) {
+    Q.spread([getBlueprints(), C.authorize_req(req)], function(blueprints, auth) {
         if (!containerAuthorized(inventoryID, auth.account)) {
             return res.sendStatus(401);
         }
 
+        var blueprint  = blueprints[blueprintID];
+
         if (inventory === undefined || inventory.hanger[sliceID] === undefined) {
             res.status(400).send("no such inventory");
+        } else if (blueprint === undefined) {
+            res.status(400).send("invalid blueprint");
         } else {
             var slice = inventory.hanger[sliceID];
 
@@ -180,16 +242,22 @@ app.post('/ships', function(req, res) {
 
             slice[blueprintID] -= 1;
 
-            slice.unpacked.push({
+            var ship = ships[uuid] = {
                 uuid: uuid,
-                blueprint: blueprintID
-            });
+                blueprint: blueprintID,
+                location: inventoryID,
+                slice: sliceID,
+                in_space: false
+            };
 
-            buildContainer(uuid, blueprintID);
+            slice.unpacked.push(uuid);
 
-            res.sendStatus(201);
+            buildContainer(uuid, auth.account, blueprint);
+
+            res.send(ship);
         }
     }).fail(function(e) {
+        console.log(e.stack);
         res.status(500).send(e.toString());
     }).done();
 });
@@ -198,10 +266,10 @@ app.post('/ships', function(req, res) {
 app.post('/inventory', function(req, res) {
     Q.spread([getBlueprints(), C.authorize_req(req)], function(blueprints, auth) {
         var dataset = req.body,
-            transactions = [],
-            containers = [],
-            new_containers = [],
-            old_containers = [];
+        transactions = [],
+        containers = [],
+        new_containers = [],
+        old_containers = [];
 
         dataset.forEach(function(t) {
             t.blueprint = blueprints[t.blueprint];
@@ -217,8 +285,8 @@ app.post('/inventory', function(req, res) {
             //yet. But that's ok because the balanced transactions below
             //make sure that it must already exist to be deployed.
             /*if (auth.priviliged === true) {
-                // Because spodb does it when it deploys things from inventory
-                throw new Error("not authorized to create containers");
+            // Because spodb does it when it deploys things from inventory
+            throw new Error("not authorized to create containers");
             }*/
 
             if (t.container_action == "create") {
@@ -240,20 +308,31 @@ app.post('/inventory', function(req, res) {
             if (old_containers.indexOf(t.inventory) > 0) {
                 throw new Error(t.inventory + " is being deleted");
             } else if (new_containers.indexOf(t.inventory) == -1 &&
-                !containerAuthorized(t.inventory, auth.account)) {
+                       !containerAuthorized(t.inventory, auth.account)) {
                 throw new Error(auth.account + " cannot access " + t.inventory);
+            }
+
+            if (t.ship_uuid !== undefined) {
+                var shipRecord = ships[t.ship_uuid];
+                if (shipRecord === undefined) {
+                    throw new Error("no such ship: "+t.ship_uuid);
+                } else if(shipRecord.in_space === true) {
+                    throw new Error("that ship is in space and cannot be moved");
+                } else {
+                    t.blueprint = blueprints[shipRecord.blueprint];
+                }
+
+                if (t.quantity === -1) {
+                    if (shipRecord.location !== t.inventory || shipRecord.slice !== t.slice) {
+                        throw new Error("the ship is not there");
+                    }
+                } else if (t.quantity != 1) {
+                    throw new Error("quantity must be 1 or -1 for unpacked ships");
+                }
             }
 
             if (t.blueprint === undefined || (t.blueprint.volume === undefined)) {
                 throw new Error("invalid blueprint: " + t.blueprint);
-            }
-
-            if (t.ship_uuid !== undefined) {
-                // TODO lookup the blueprint we have stored
-
-                if (t.quantity != -1 && t.quantity != 1) {
-                    throw new Error("quantity must be 1 or -1 for unpacked ships");
-                }
             }
 
             transactions.push(t);
@@ -307,6 +386,7 @@ app.post('/inventory', function(req, res) {
     }).done();
 });
 
+// TODO if there is not enough room, the transaction will fail unbalanced
 function executeTransfers(transfers) {
     transfers.forEach(function(transfer) {
         var example = {
@@ -316,11 +396,13 @@ function executeTransfers(transfers) {
             blueprint: {},
             ship_uuid: 'uuid' // only for unpacked ships and quantity must == -1 or 1
         };
+        console.log(transfer);
 
         var slot;
         var inventory = inventories[transfer.inventory];
         var quantity = transfer.quantity;
         var sliceID = transfer.slice;
+
         var type = transfer.blueprint.uuid;
 
         if (inventory === undefined) {
@@ -354,17 +436,18 @@ function executeTransfers(transfers) {
         if (transfer.ship_uuid !== undefined) {
             var list = slice.unpacked;
             if (quantity > 0) {
-                slice.push({
-                    uuid: transfer.ship_uuid,
-                    blueprint: transfer.blueprint.uuid
-                });
-            } else {
-                var i = slice.indexOf(transfer.ship_uuid);
+                list.push(transfer.ship_uuid);
 
-                if (i == -1) {
+                ships[transfer.blueprint.uuid].location = transfer.inventory;
+                ships[transfer.blueprint.uuid].slice = transfer.slice;
+            } else {
+                var i = list.indexOf(transfer.ship_uuid);
+                var shipRecord = ships[transfer.ship_uuid];
+
+                if (i == -1 || shipRecord.location !== transfer.inventory || shipRecord.slice !== transfer.slice) {
                     throw new Error("ship not present in hanger: " + transfer.ship_uuid);
                 } else {
-                    slice.splice(i, 1);
+                    list.splice(i, 1);
                 }
             }
         } else {
